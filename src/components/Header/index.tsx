0bb4237503175f5020eb572daa9da0c70a32512e
Leandro Citro De Andrade <leandro_citro@hotmail.com>
2023-10-20 11:33:16 -0300

    Adding Modal New Transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles"
 import logoImg from '../../assets/Logo.svg'
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function Header() {
     return(
@@ -8,7 +9,21 @@ export function Header() {
             <HeaderContent>
                 <img src={logoImg} alt="" />
 
-                <NewTransactionButton> Nova transação </NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                    <NewTransactionButton> Nova transação </NewTransactionButton>
+                    </Dialog.Trigger>
+                
+
+                <Dialog.Portal>
+                    <Dialog.Overlay />
+                    <Dialog.Content>
+                        <Dialog.Title> Nova Transação </Dialog.Title>
+                        <Dialog.Close />
+                    </Dialog.Content>
+                </Dialog.Portal>
+                </Dialog.Root>
+
             </HeaderContent>
         </HeaderContainer>
         </div>
